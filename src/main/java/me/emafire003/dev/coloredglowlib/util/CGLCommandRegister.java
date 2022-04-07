9778c813ce8aac426d2db1f39f8922ce372217b4
Emafire003 <29462910+Emafire003@users.noreply.github.com>
2022-04-07 10:25:55 +0200

    Added /setglowcolor for EntityTypes

--- src/main/java/me/emafire003/dev/coloredglowlib/util/CGLCommandRegister.java
+++ src/main/java/me/emafire003/dev/coloredglowlib/util/CGLCommandRegister.java
@@ -1,6 +1,7 @@
 package me.emafire003.dev.coloredglowlib.util;
 
 import me.emafire003.dev.coloredglowlib.command.SetGlowingColor;
+import me.emafire003.dev.coloredglowlib.command.SetTypeGlowingColor;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 
 import static me.emafire003.dev.coloredglowlib.ColoredGlowLib.LOGGER;
@@ -9,6 +10,7 @@ public class CGLCommandRegister {
     public static void registerCommands() {
         LOGGER.info("Registering commands...");
         CommandRegistrationCallback.EVENT.register(SetGlowingColor::register);
+        CommandRegistrationCallback.EVENT.register(SetTypeGlowingColor::register);
         LOGGER.info("Done!");
     }
 }
